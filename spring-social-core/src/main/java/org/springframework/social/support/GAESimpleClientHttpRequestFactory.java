d3f32e0232e0a4c0ec116273ee27b1704ca7aab2
Scott Murphy <scott@alwaysvip.com>
2011-12-14 16:46:50 -0800

    Increase timeouts from 10 seconds to 45 seconds.

--- spring-social-core/src/main/java/org/springframework/social/support/GAESimpleClientHttpRequestFactory.java
+++ spring-social-core/src/main/java/org/springframework/social/support/GAESimpleClientHttpRequestFactory.java
@@ -11,13 +11,13 @@ import java.net.URL;
 // By default, the deadline for a fetch is 5 seconds. The maximum deadline is 10 seconds for HTTP requests and 10 minutes for task queue and cron job requests. 
 // When using the URLConnection interface, the service uses the connection timeout (setConnectTimeout()) plus the read timeout (setReadTimeout()) as the deadline.
 
-// The code below would set a maximum of 20 seconds to be safe as I am not sure by setting 5 each one might be a limiting factor.
+// The code below would set a maximum of 90 seconds to be safe as I am not sure by setting 45 each one might be a limiting factor.
 
 public class GAESimpleClientHttpRequestFactory extends SimpleClientHttpRequestFactory {
     @Override
     protected void prepareConnection(HttpURLConnection connection, String httpMethod) throws IOException {
-        connection.setConnectTimeout(10000);
-		connection.setReadTimeout(10000);
+        connection.setConnectTimeout(45000);
+		connection.setReadTimeout(45000);
         super.prepareConnection(connection, httpMethod);
     }
 }
